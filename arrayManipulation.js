45703e9510e456584549ef6390b46f9fff9948b8
Nana Ampomah Arhin <anarhin002@st.ug.edu.gh>
2024-05-17 16:56:11 -0700

    added the formatArrayStrings function

--- arrayManipulation.js
+++ arrayManipulation.js
@@ -4,3 +4,12 @@ function processArray(arrayOfNumbers) {
     return number % 2 === 0 ? number * number : number * 3;
   });
 }
+
+//a function that converts strings to uppercase or lower case based on its corresponding number in arrayOfNumbers
+function formatArrayStrings(arrayOfStrings, arrayOfNumbers) {
+  return arrayOfStrings.map((string, index) => {
+    return arrayOfNumbers[index] % 2 === 0
+      ? string.toUpperCase()
+      : string.toLowerCase();
+  });
+}
